d25857b4ee658fb15fa4e4f906ef58e180770564
dan4in <53431991+dan4in@users.noreply.github.com>
2023-03-22 12:39:49 +0300

    Add files via upload

--- v.vb
+++ v.vb
@@ -1,3 +1,3 @@
 This code sets up the content script for the extension. The `isPageBright` function determines whether the page is bright or not by calculating the brightness of the body background color and text color. The `getBrightness` function calculates the brightness of a color in the RGB format. The `chrome.storage.sync.get` method retrieves the `isDarkModeEnabled` setting from the storage and checks if it's enabled and the page is not already in dark mode. If so, it executes the `toggleDarkMode` function.
 
-
+5. Create a popup.html file and add the following code:
